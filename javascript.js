25e520f1a05f5f72882ae9a021661210c764a5b1
MD ZAKIR HOSSAIN <159409026+zakirSIR@users.noreply.github.com>
2024-03-30 17:34:49 +0600

    Add files via upload
    I have completed the personal project

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+//lekha asbe abar muche jabe
+var typed =new Typed(".text",{
+    Strings:["Front-end Webdeveloper" , "Youtuber" , "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
